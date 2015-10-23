f114ebff9793f05c22b87680a38250cc6d8258d1
pulkitparashar7 <pulkitparashar7@gmail.com>
2015-10-23 14:19:43 +0530

    Update gps_track.js

--- script/gps_track.js
+++ script/gps_track.js
@@ -1,17 +1,17 @@
-﻿var latituda:float;
-var longituda:float;
+var latitud:float;
+var longitud:float;
 private var  gps : boolean ;
 var windowRect : Rect; 
  var guiText: GUIText;
 var stringToEdit : String ;
-// Use this for initialization
+
 
 
 function Start () {
 windowRect =  Rect (40, 20, 400, 300);
-// Disable screen dimming
+
 Screen.sleepTimeout = SleepTimeout.NeverSleep;
-// First, check if user has location service enabled
+
 if (!Input.location.isEnabledByUser)
 {
 	gps=false;
@@ -19,9 +19,7 @@ if (!Input.location.isEnabledByUser)
 }
 
 
-// Start service before querying location
 Input.location.Start (10.0f,1.0f);
-// Wait until service initializes
 var maxWait : int = 40;
 while (Input.location.status== LocationServiceStatus.Initializing && maxWait > 0) 
 {
@@ -29,14 +27,13 @@ yield WaitForSeconds (1);
 maxWait--;
 }
 
-// Service didn't initialize in xx seconds
 if (maxWait < 1) 
 {
 print ("Timed out");
 return;
 }
 
-// Connection has failed
+
 if (Input.location.status == LocationServiceStatus.Failed) 
 {
 print ("Unable to determine device location");
@@ -69,8 +66,8 @@ li= Input.location.lastData;
 lat = li.latitude;
 lon = li.longitude;
 
-latituda=lat;
-longituda=lon;
+latitud=lat;
+longitud=lon;
 }
 
 
@@ -82,7 +79,7 @@ function OnGUI ()
 	}
 	else if (gps== true)
 	{
-		GUI.Button (new Rect (10,10,150,60), "LAT= "+ latituda +"\nLON= " + longituda);
+		GUI.Button (new Rect (10,10,150,60), "LAT= "+ latitud +"\nLON= " + longitud);
 		GUI.Button (new Rect (10,70,150,60), "STAT= " +Input.location.status+"\nON= " + Input.location.isEnabledByUser);
 	}
 }
@@ -91,5 +88,5 @@ function OnGUI ()
 	{
 	 	GUI.Label (Rect (100, 100, 200, 100), " Enable gps to continue.");
 		GUI.Button (Rect (270,250,100,20), "Ok");
-			//print ("Got a click");
+			
 	}
